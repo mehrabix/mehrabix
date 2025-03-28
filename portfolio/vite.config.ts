fd703b2f3d629aa94f76c7706943537d0aea3563
mehrabix <mehrabi@post.com>
2025-03-28 18:55:49 +0330

    Update GitHub Pages configuration

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // This will be your repository name
+  base: '/mehrabix/', // Updated to match your repository name
 })
